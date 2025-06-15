57579875521b664a26de546477f9450e44b0046b
Rishita-Garg <125175733+Rishita-Garg@users.noreply.github.com>
2025-06-15 21:22:42 +0530

    day 6 q4

---
+++ implement_queue_using_stacks.py
@@ -0,0 +1,24 @@
+# Problem: Implement Queue using Stacks (Leetcode #232)
+# Link: https://leetcode.com/problems/implement-queue-using-stacks/
+
+class MyQueue:
+
+    def __init__(self):
+        self.input_stack = []
+        self.output_stack = []
+
+    def push(self, x: int) -> None:
+        self.input_stack.append(x)
+
+    def pop(self) -> int:
+        self.peek()  # Ensure output_stack has the current front
+        return self.output_stack.pop()
+
+    def peek(self) -> int:
+        if not self.output_stack:
+            while self.input_stack:
+                self.output_stack.append(self.input_stack.pop())
+        return self.output_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.input_stack and not self.output_stack
